f6ba09755cc0e542771b1c2a3a13ece3eda2edf1
pratiknarkhede <pratiknarkhede646@gmail.com>
2022-09-19 16:21:29 +0530

    corrected class name

--- 04_toset/CollectExample.java
+++ 04_toset/CollectExample.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import static java.util.stream.Collectors.*;
 
-public class CollectExample {  
+public class CollectToSet {  
   public static void main(String[] args) {
     List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
